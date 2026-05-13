513ed9ea35978469a0328f17ef87bd6b4e0459c3
Wesley Silva <wesley_developer@outlook.com>
2026-05-13 06:45:25 -0300

    fix: adicionando suporte para o laravel 12

--- src/PubSubQueue.php
+++ src/PubSubQueue.php
@@ -92,6 +92,54 @@ class PubSubQueue extends Queue implements QueueContract
     }
 
     /**
+     * Get the number of pending jobs for the queue.
+     * PubSubClient has no method to retrieve this data.
+     *
+     * @param  string|null  $queue
+     * @return int
+     */
+    public function pendingSize($queue = null)
+    {
+        return 0;
+    }
+
+    /**
+     * Get the number of delayed jobs for the queue.
+     * PubSubClient has no method to retrieve this data.
+     *
+     * @param  string|null  $queue
+     * @return int
+     */
+    public function delayedSize($queue = null)
+    {
+        return 0;
+    }
+
+    /**
+     * Get the number of reserved jobs for the queue.
+     * PubSubClient has no method to retrieve this data.
+     *
+     * @param  string|null  $queue
+     * @return int
+     */
+    public function reservedSize($queue = null)
+    {
+        return 0;
+    }
+
+    /**
+     * Get the creation timestamp of the oldest pending job.
+     * Not supported by PubSub.
+     *
+     * @param  string|null  $queue
+     * @return int|null
+     */
+    public function creationTimeOfOldestPendingJob($queue = null)
+    {
+        return null;
+    }
+
+    /**
      * Push a new job onto the queue.
      *
      * @param  string|object  $job
